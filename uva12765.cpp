7c3eb406fa5e434f7738b29bc444d11f1ad7c80c
kenny181920 <kmollapazam@ulasalle.edu.pe>
2018-05-06 21:39:52 -0500

    Corregido

--- uva12765.cpp
+++ uva12765.cpp
@@ -1,44 +1,41 @@
 #include <iostream>
 using namespace std;
 
-unsigned factorial(unsigned n)
-{
-    if(n>1){
-    	return n*factorial(n-1);
-    }
-    else{
-    	return 1;
-    }
-}
-
 int main(){
-	unsigned num1,num2,n,m,fact1,fact2,suma1=1,suma2=1;
+	int n,m,num1,num2;
 	while(cin>>n>>m){
-		if((n==0) and (m==0)){
+		if(n==0 or m==0){
 			break;
 		}
+
+		int numN[10]={0,0,0,0,0,0,0,0,0,0};
+		int numM[10]={0,0,0,0,0,0,0,0,0,0};
+
 		for(int i=0;i<n;i++){
 			cin>>num1;
-			fact1=factorial(num1);
-			suma1=suma1*fact1;
+			for(int j=2;j<=num1;j++){
+				numN[j]++;
+			}
 		}
-		unsigned dato1=suma1;
+
 		for(int i=0;i<m;i++){
 			cin>>num2;
-			fact2=factorial(num2);
-			suma2=suma2*fact2;
+			for(int j=2;j<=num2;j++){
+				numM[j]++;
+			}
 		}
-		unsigned dato2=suma2;
-		if((dato1==dato2) and (dato1!=0) and (dato2!=0)){
-			printf("%s\n","YES");
-			suma1=1;
-			suma2=1;
+
+		numN[2]+=2*numN[4]+numN[6]+3*numN[8];
+		numN[3]+=2*numN[9]+numN[6];
+		numN[4]=numN[6]=numN[8]=numN[9]=0;
+		numM[2]+=2*numM[4]+numM[6]+3*numM[8];
+		numM[3]+=2*numM[9]+numM[6];
+		numM[4]=numM[6]=numM[8]=numM[9]=0;
+		int op=1;
+		for(int i=0;i<10;i++){
+			op&=numN[i]==numM[i];
 		}
-		else{
-			printf("%s\n","NO");
-			suma1=1;
-			suma2=1;
-		}
+		printf("%s\n",op? "YES" : "NO");
 	}
 	return 0;
 }
